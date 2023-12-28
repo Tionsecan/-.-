d171d271a163a24c48de137d410d11b563354518
Tionsecan <154830548+Tionsecan@users.noreply.github.com>
2023-12-28 17:04:51 +0300

    8.py

--- 8.py
+++ 8.py
@@ -1,9 +1,10 @@
-from math import ceil
- 
-def Оплата(d):
-    return base+ceil(d/140)*add
- 
-base = 240
-add = 15
-distance = int(input("Введите расстояние: "))
-print('Стоимость поездки: ', Оплата(distance))
+def calculate_taxi_payment(distance):
+    тариф = 240
+    за140м = 15
+    distance_in_140m = distance * 1000 / 140
+    additional_fare = за140м * distance_in_140m
+    сумма = тариф + additional_fare
+    return сумма
+distance = int(input("введите расстояние в км. "))
+payment = calculate_taxi_payment(distance)
+print("Итоговая сумма оплаты", round((payment),1), "p.")
